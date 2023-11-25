34e69efdf45911fc4c2202dafb3879bd77b8dd83
Simo672k <mohammed.hakmiii555@gmail.com>
2023-11-25 14:26:42 +0100

    playing around with controllers

---
+++ src/book/book.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Param } from "@nestjs/common";
+
+@Controller('book')
+export class BookController {
+  @Get("")
+  getAllBooks(){
+    return "List of all books";
+  }
+
+  @Get(":id")
+  getBook(@Param("id") id:string){
+    return {
+      bookId: id,
+      bookData:{
+        bookTitle: "Harry Potter",
+        bookChapters: 25,
+        bookPublisher: "Jhon Doe",
+        bookTags: ["Action", "Fantasy", "Adventure"],
+        bookTotalReads: 18520,
+        bookRate: 4.8
+      }
+    }
+  }
+}
