feef8d6cd2ca920c1398e00fab1353f318f2262c
Simo672k <mohammed.hakmiii555@gmail.com>
2023-11-25 15:10:53 +0100

    Dto class validation with decorators and validation pipes

--- src/book/dto/createNewBook.ts
+++ src/book/dto/createNewBook.ts
@@ -1,5 +1,12 @@
+import { IsString, IsNumber } from "class-validator"
+
 export class CreateNewBookDTO {
+  @IsString()
   bookTitle: string
+
+  @IsString()
   bookAuthor: string
+  
+  @IsNumber()
   bookChapter: number
 }
